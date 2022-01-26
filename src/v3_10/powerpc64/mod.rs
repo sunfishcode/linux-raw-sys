54cc3c88194729826afbd17b70c6bb5f72eb839b
Dan Gohman <dev@sunfishcode.online>
2022-01-26 11:35:51 -0800

    Add a new ioctl module, and populate it with lots of ioctl codes. (#18)
    We've been collecting manual definitions of ioctl codes for a while, and
    it looks like we're going to be collecting more over time, so start trying
    a different strategy: use a script and a C program to extract ioctl values
    from the headers semi-automatically.
    
    The script itself isn't very easy to run, and I'm open to ideas for how
    to do it better. However, this should cover most of Linux's existing ioctls,
    so hopefully we won't need to run it very often.

--- src/v3_10/powerpc64/mod.rs
+++ src/v3_10/powerpc64/mod.rs
@@ -4,6 +4,9 @@ pub mod r#errno;
 /// modules/general.h
 #[cfg(feature = "general")]
 pub mod r#general;
+/// modules/ioctl.h
+#[cfg(feature = "ioctl")]
+pub mod r#ioctl;
 /// modules/netlink.h
 #[cfg(feature = "netlink")]
 pub mod r#netlink;
