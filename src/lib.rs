ae26aae641d9c78825e81600c452c332a5671c9d
jtnunley <jtnunley01@gmail.com>
2022-12-06 14:10:06 -0800

    Add CMSG macros

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,59 @@ impl PartialEq for general::__kernel_timespec {
 #[cfg(feature = "general")]
 impl Eq for general::__kernel_timespec {}
 
+mod cmsg_macros {
+    use crate::ctypes::{c_long, c_uint, c_uchar};
+    use crate::general::{cmsghdr, msghdr};
+    use core::mem::size_of;
+    use core::ptr;
+
+    pub unsafe fn CMSG_ALIGN(len: c_uint) -> c_uint {
+        let c_long_size = size_of::<c_long>() as c_uint;
+        (len + c_long_size - 1) & !(c_long_size - 1)
+    }
+
+    pub unsafe fn CMSG_DATA(cmsg: *const cmsghdr) -> *mut c_uchar {
+        (cmsg as *mut c_uchar).offset(size_of::<cmsghdr>() as isize)
+    }
+
+    pub unsafe fn CMSG_SPACE(len: c_uint) -> c_uint {
+        size_of::<cmsghdr>() as c_uint + CMSG_ALIGN(len)
+    }
+
+    pub unsafe fn CMSG_LEN(len: c_uint) -> c_uint {
+        size_of::<cmsghdr>() as c_uint + len
+    }
+
+    pub unsafe fn CMSG_FIRSTHDR(mhdr: *const msghdr) -> *mut cmsghdr {
+        if (*mhdr).msg_controllen < size_of::<cmsghdr>() as _ {
+            return ptr::null_mut();
+        }
+
+        (*mhdr).msg_control as *mut cmsghdr
+    }
+
+    pub unsafe fn CMSG_NXTHDR(mhdr: *const msghdr, cmsg: *const cmsghdr) -> *mut cmsghdr {
+        // TODO: This function should observe Rust's provenance rules once they're set in stone.
+
+        let cmsg_len = (*cmsg).cmsg_len;
+        let next_cmsg = (cmsg as *mut u8).offset(CMSG_ALIGN(cmsg_len as _) as isize) as *mut cmsghdr;
+        let max = ((*mhdr).msg_control as usize) + ((*mhdr).msg_controllen as usize);
+
+        if cmsg_len < size_of::<cmsghdr>() as _ {
+            return ptr::null_mut();
+        }
+
+        if next_cmsg.offset(1) as usize > max || next_cmsg as usize + CMSG_ALIGN(cmsg_len as _) as usize > max
+        {
+            return ptr::null_mut();
+        }
+
+        next_cmsg
+    }
+}
+
+pub use cmsg_macros::*;
+
 // The rest of this file is auto-generated!
 #[cfg(feature = "errno")]
 #[cfg(target_arch = "arm")]
