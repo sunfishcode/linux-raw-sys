7e4d7ad69be3fb687bdc45df1d49ec1161c6b195
Kristian Høgsberg Kristensen <krh@bitplanet.net>
2023-11-30 08:12:44 -0800

    Add mempolicy.h definition (#96)
    Co-authored-by: Kristian H. Kristensen <krh@rivosinc.com>

--- gen/modules/general.h
+++ gen/modules/general.h
@@ -16,6 +16,7 @@
 #include <linux/inotify.h>
 #include <linux/limits.h>
 #include <linux/magic.h>
+#include <linux/mempolicy.h>
 #include <linux/mman.h>
 #include <linux/poll.h>
 #include <linux/random.h>
