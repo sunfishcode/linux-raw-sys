58822b5ab5bef12ea5aa385b58f8271a72924f70
Raito Bezarius <masterancpp@gmail.com>
2024-02-17 20:03:50 +0100

    feat: add support for `mtd` UAPI
    This adds MTD (`mtd/mtd-user.h`) UAPI pieces, useful to manipulate MTD devices
    from Rust.
    
    This adds also a few `ioctl` exposed by this userspace API.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

---
+++ gen/modules/mtd.h
@@ -0,0 +1,9 @@
+// This file includes selected Linux header files.
+
+#include "support.h"
+
+// Selected Linux headers.
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,10,0)
+#include <mtd/mtd-user.h>
+#endif
