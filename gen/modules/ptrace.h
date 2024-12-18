59c392fa9f5d6071aa3b956548fb7387388670c4
Yulia 🥺 <44705138+yuki0iq@users.noreply.github.com>
2024-12-18 04:22:14 +0300

    Add ptrace and seccomp bindings (#136)

---
+++ gen/modules/ptrace.h
@@ -0,0 +1,6 @@
+#include "support.h"
+
+#include <linux/audit.h>   /* Definition of AUDIT_* constants */
+#include <linux/filter.h>  /* Definition of struct sock_fprog */
+#include <linux/ptrace.h>  /* Definition of PTRACE_* constants */
+#include <linux/seccomp.h> /* Definition of SECCOMP_* constants */
