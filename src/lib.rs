61eab2a91696c94864cbe6233c45fc125d53b205
Henry Chen <chenx97@aosc.io>
2025-04-09 18:10:39 +0800

    Add c_char definition for MIPS R6 (#154)

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,8 @@ pub mod ctypes {
         target_arch = "loongarch64",
         target_arch = "mips",
         target_arch = "mips64",
+        target_arch = "mips32r6",
+        target_arch = "mips64r6",
         target_arch = "sparc",
         target_arch = "sparc64",
         target_arch = "x86",
